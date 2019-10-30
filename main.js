4e30ce8157c38116b295d78d8b9cc45f95118bfb
skjwise <skjwise@hotmail.com>
2019-10-30 15:51:50 +0000

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
 
+function init(){
+  modal.className = "hidden"
+  const hearts = document.querySelectorAll(".like-glyph")
+  hearts.forEach(function(heart){
+    heart.addEventListener("click", e => e.target.className === "activated-heart"
+    ? unlike(e)
+    : like(e)
+    )
+  })
+}
 
+function like(e){
+  mimicServerCall().then(
+    e.target.className = "activated-heart",
+    e.target.textContent = FULL_HEART
+    )
+    .catch(
+      error => {
+        const modalMessage = modal.querySelector("#modal-message")
+        modalMessage.innerText = error.message
+        modal.className = ""
+        setTimeout( () => modal.className = "hidden", 5000)
+      }
+    )
+}
+
+function unlike(e){
+    e.target.className = "",
+    e.target.textContent = EMPTY_HEART
+}
+
+init()
 
 
 //------------------------------------------------------------------------------
